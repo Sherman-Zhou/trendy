2c3a39a79daa7f9044d5d40d2c7b680a5ac1725f
junwei <sjwhsh@163.com>
2020-06-28 10:29:16 +0800

    fix permission

--- src/main/java/com/joinbe/config/SecurityConfiguration.java
+++ src/main/java/com/joinbe/config/SecurityConfiguration.java
@@ -97,7 +97,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
-            .antMatchers("/account/forgot-password/**").permitAll()
+            .antMatchers("/account/forgot-password/*/**").permitAll()
             .antMatchers("/account/forgot-password/finish").permitAll()
             .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
